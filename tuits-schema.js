4a0addbdd039a874e1027e904329da1c87e3439d
Helen Z <zhengxiuge@gmail.com>
2022-04-13 15:32:25 -0400

    a9 version 3

--- tuits-schema.js
+++ tuits-schema.js
@@ -6,10 +6,20 @@ shown below implements a schema for the tuits collection we worked on earlier.
 import mongoose from 'mongoose';        // load mongoose library
 const schema = mongoose.Schema(         // create schema
     {
-        tuit: String,                   // tuit property of type String
-        likes: Number,                  // likes property of type Number
-        postedBy: {
-            username: String
-        }
+            tuit: String,               // tuit property of type String
+            stats: {
+                    retuits: Number,
+                    comments: Number,
+                    likes: Number,         // likes property of type Number
+                    dislikes: Number,
+            },
+            liked: Boolean,
+            disliked: Boolean,
+            logo_image: String,
+            postedBy: {
+                    username: String
+            },
+            time: String,
+            handle: String
         }, {collection: 'tuits'});      // which collection name
 export default schema;                  // export schema so it can be used elsewhere
